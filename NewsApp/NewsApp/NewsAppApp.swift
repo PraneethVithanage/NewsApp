73e050fce06372c77c0b22b655f6fc8823ea4557
Praneeth Vithanage <praneethvithanage10@gmail.com>
2022-08-19 22:39:30 +0530

    Project Creation
    Creation: Create NewsApp project

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by MacBook on 2022-08-19.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
